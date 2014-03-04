0050e6e961a35258861ffaa921ad6501e5bedd56
bsouza <to.bsouza@gmail.com>
2014-03-04 06:42:46 -0300

    basic rules

--- lib/soundex.rb
+++ lib/soundex.rb
@@ -1,2 +1,32 @@
 class Soundex
+
+  def encode(word)
+    head(word) + encoded_tail(word)
+  end
+
+private
+
+  TAIL_MAX_SIZE = 3
+
+  def head(word)
+    word[0]
+  end
+
+  def tail(word)
+    word[1..-1]
+  end
+
+  def encoded_tail(word)
+    encoded = String.new
+    tail(word).each_char do |char| 
+      break if is_full? encoded
+      encoded += (char != "A") ? char : "0"
+    end
+    encoded
+  end
+
+  def is_full?(encoded)
+    encoded.size >= TAIL_MAX_SIZE
+  end
+
 end
--- test/soundex_test.rb
+++ test/soundex_test.rb
@@ -3,4 +3,24 @@ require_relative '../lib/soundex'
 
 describe Soundex do
   
+  before do
+    @soundex = Soundex.new
+  end
+
+  describe 'basic rules' do
+
+    it "should have one letter followed by three numerical digits" do
+      encoded = @soundex.encode("BAAA")
+      encoded[0].must_equal "B"
+      encoded[1].must_equal "0"
+      encoded[2].must_equal "0"
+      encoded[3].must_equal "0"
+    end
+
+    it "should have 4 chars" do
+      @soundex.encode("Bclasd").size.must_equal 4
+    end
+
+  end
+
 end
